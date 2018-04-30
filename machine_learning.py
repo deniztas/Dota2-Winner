ef11923d23659e49960d129518a43dda4d6880b0
vonReany <enescankiri@gmail.com>
2018-04-30 12:05:35 +0300

    ML Algorithms change

--- machine_learning.py
+++ machine_learning.py
@@ -39,49 +39,27 @@ testDF_feature = [testDF[col].astype('category') for col in testDF.columns[1:]]
 testDF_feature = pd.DataFrame(testDF_feature).T
 
 models = [
-    ('GradientBoostingClassifier', GradientBoostingClassifier(learning_rate=0.01, n_estimators=100), 1),
-    ('LinearSVC', LinearSVC(C=1.0), 1),
-    ('SVC', SVC(C=1.0, degree=3), 1),
     ('DecisionTreeClassifier', DecisionTreeClassifier(criterion='gini'), 2),
-    ('ExtraTreeClassifier', ExtraTreeClassifier(criterion='gini'), 2),
     ('RandomForestClassifier', RandomForestClassifier(n_estimators=10, criterion='gini', n_jobs=1), 2),
-    ('CalibratedClassifierCV', CalibratedClassifierCV(method='sigmoid', cv=10), 1),
-    ('DummyClassifier', DummyClassifier(strategy='stratified'), 2),
-    ('AdaBoostClassifier', AdaBoostClassifier(n_estimators=50, learning_rate=0.1, algorithm='SAMME.R'), 1),
-    ('BaggingClassifier', BaggingClassifier(n_estimators=10, n_jobs=1), 1),
-    ('ExtraTreesClassifier', ExtraTreesClassifier(n_estimators=10, criterion='gini'), 2),
-    ('LogisticRegression', LogisticRegression(penalty='l2', max_iter=100, solver='liblinear'), 1),
-    ('LogisticRegressionCV', LogisticRegressionCV(Cs=10, penalty='l2', max_iter=100, solver='lbfgs'), 1),
-    ('RidgeClassifier', RidgeClassifier(alpha=1.0), 1),
-    ('RidgeClassifierCV', RidgeClassifierCV(alphas=(0.1, 1.0, 10.0), cv=10), 2),
-    ('SGDClassifier', SGDClassifier(loss='hinge', penalty='l2', epsilon=0.1, n_jobs=1), 1),
-    ('BernoulliNB', BernoulliNB(alpha=1.0), 1),
-    ('MultinomialNB', MultinomialNB(alpha=1.0), 1),
     ('KNeighborsClassifier', KNeighborsClassifier(n_neighbors=5, algorithm='auto', metric='minkowski', n_jobs=1), 2),
-    ('RadiusNeighborsClassifier', RadiusNeighborsClassifier(radius=100.0, weights='uniform', algorithm='auto'), 2),
-    ('MLPClassifier', MLPClassifier(hidden_layer_sizes=(100, ), activation='relu', solver='adam', alpha=0.0001, learning_rate_init=0.001, max_iter=200), 2),
-    ('MLPClassifier', MLPClassifier(hidden_layer_sizes=(256, ), activation='relu', solver='adam', alpha=0.0001, learning_rate_init=0.0001, max_iter=2000), 2),
     ('MLPClassifier', MLPClassifier(hidden_layer_sizes=(16, 32, 16 ), activation='relu', solver='adam', alpha=0.0001, learning_rate_init=0.001, max_iter=200), 2),
-    ('MLPClassifier', MLPClassifier(hidden_layer_sizes=(50, 50 ), activation='relu', solver='adam', alpha=0.0001, learning_rate_init=0.001, max_iter=200), 2),
     ('MLPClassifier', MLPClassifier(hidden_layer_sizes=(128, 256, 128 ), activation='relu', solver='adam', alpha=0.0001, learning_rate_init=0.001, max_iter=200), 2),
-    ('LabelPropagation', LabelPropagation(kernel='rbf', n_neighbors=7, max_iter=1000), 1),
+    ('MLPClassifier', MLPClassifier(hidden_layer_sizes=(64, 128, 256, 128, 64 ), activation='relu', solver='adam', alpha=0.0001, learning_rate_init=0.001, max_iter=100), 2),
 ]
 
 for i, mod in enumerate(models):
     try:
-        trainTarget = trainDF_target if mod[2] == 1 else trainDF_target_dummy
-        testTarget = testDF_target if mod[2] == 1 else testDF_target_dummy
-        embed()
+        trainTarget = trainDF_target_dummy
+        testTarget = testDF_target_dummy
 
         ### Step 3) Machine Learning Model
-        print("====== Step 3.%s : Fitting Model ======"%(str(i)))
+        print("====== Step 3.%s : Fitting %s Model ======"%(str(i), mod[0]))
         classModel = mod[1]
         classModel.fit(X = trainDF_feature, y = trainTarget)
 
         ### Step 4) Calculating Accuracy
         print("====== Step 4.%s : Accuracy Calculation ======"%(str(i)))
         pred = classModel.predict(testDF_feature)
-        embed()
         targetWin = [col.argmax() for col in testTarget.as_matrix()]
         predWin = [col.argmax() for col in pred]
         print("Accuracy of %s: %.2f" %(mod[0], accuracy_score(targetWin, predWin)))
